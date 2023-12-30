6144b1d40bce0e7a9c7975b2333e1d7264342217
Elli <114315756+0bbedCode@users.noreply.github.com>
2023-12-30 17:42:34 -0500

    Another small fix
    Fixed NULL Concatenation on local

--- app/src/main/assets/files/id_file_exists.lua
+++ app/src/main/assets/files/id_file_exists.lua
@@ -4,8 +4,13 @@ function before(hook, param)
         return false
     end
 
-    if param:isDriverFile(ths:getPath()) then
-        log("File.exists() IS Driver, Returning False... [" .. arg1 .. "]")
+    local path = ths:getPath()
+    if path == nil then
+        return false
+    end
+
+    if param:isDriverFile(path) then
+        log("File.exists() IS Driver, Returning False... [" .. path .. "]")
         param:setResult(false)
         return true
     end
